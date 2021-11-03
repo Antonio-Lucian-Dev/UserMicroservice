7e63454c0bae218f3fc179cd8acd9ef517f625a8
Antonio Lucian <web.antonio.popa@gmail.com>
2021-11-03 17:00:01 +0200

    Modifies sql file

--- src/main/resources/db/userandaddress.sql
+++ src/main/resources/db/userandaddress.sql
@@ -1,18 +1,18 @@
 DROP TABLE if exists Address cascade;
 DROP TABLE if exists UserAddresses cascade;
 DROP TABLE if exists Users cascade;
+DROP TABLE if exists t cascade;
 
 CREATE TABLE Address(
-    id uuid PRIMARY KEY NOT NULL,
-    city varchar(50) NOT NULL,
-    country varchar(50) not null,
-    zip_code int not null,
-    street varchar(50),
-    street_number int
+    id uuid DEFAULT gen_random_uuid () PRIMARY KEY ,
+    city varchar(250),
+    country varchar(250),
+    zip_code text,
+    county varchar(50)
 );
 
 CREATE TABLE UserAddresses(
-    id uuid PRIMARY KEY NOT NULL,
+    id uuid DEFAULT gen_random_uuid () PRIMARY KEY ,
     street varchar(50),
     street_number int,
     address uuid,
@@ -21,7 +21,7 @@ CREATE TABLE UserAddresses(
 );
 
 CREATE TABLE Users(
-    id uuid PRIMARY KEY,
+    id uuid DEFAULT gen_random_uuid () PRIMARY KEY ,
     first_name varchar(50) not null,
     last_name varchar(50) not null,
     email varchar(50) not null,
@@ -29,18 +29,15 @@ CREATE TABLE Users(
     password varchar(50) not null,
     address_id uuid,
     constraint FK_UserAddress FOREIGN KEY (address_id)
-    REFERENCES Address(id)
+    REFERENCES UserAddresses(id)
 );
 
+create table t (COUNTRY varchar(50), POSTAL_CODE text, CITY varchar(255), STATE varchar(255), SHORT_STATE varchar(50), COUNTY varchar(50), SHORT_COUNTY text, COMMUNITY varchar(255),
+               SHORT_COMUNITY text, LATITUDE text, LONGITUDE text, ACCURACY text);
+copy t(COUNTRY, POSTAL_CODE, CITY, STATE, SHORT_STATE, COUNTY, SHORT_COUNTY, COMMUNITY, SHORT_COMUNITY, LATITUDE, LONGITUDE, ACCURACY) from 'C:\Users\Asu\Downloads\allCountriesCSV.csv' CSV HEADER DELIMITER ',';
 
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b5c6-4896-987c-f30f3678f608', 'Adjud', 'Romania', 00170);
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b5c6-4896-987c-f30f3678f609', 'Agas', 'Romania', 607005);
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b5c6-4896-987c-f30f3678f606', 'Apa Asau', 'Romania', 607021);
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b5c6-4896-987c-f30f3678f708', 'Asau', 'Romania', 607020);
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b5c6-4896-987c-f30f3679f608', 'Albele', 'Romania', 607036);
-INSERT INTO Address(id, city, country, zip_code) values('40e6225d-b5c6-4896-987c-f30f3678f608', 'Ardeoani', 'Romania', 607015);
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b5c6-5896-987c-f30f3678f608', 'Antohesti', 'Romania', 607261);
-INSERT INTO Address(id, city, country, zip_code) values('40e3215d-b5c6-4896-987c-f30f3678f608', 'Arini', 'Romania', 607196);
-INSERT INTO Address(id, city, country, zip_code) values('40e6216d-b5c6-4896-987c-f30f3678f608', 'Bacau', 'Romania', 600408);
-INSERT INTO Address(id, city, country, zip_code) values('40e1215d-b5c6-4896-987c-f30f3678f608', 'Bacioiu', 'Romania', 607121);
-INSERT INTO Address(id, city, country, zip_code) values('40e6215d-b3c6-4896-987c-f30f3678f608', 'Bahnaseni', 'Romania', 607431);
+insert into address (city, country, zip_code, county)
+select DISTINCT ON (CITY) CITY, COUNTRY, POSTAL_CODE, COUNTY
+from t;
+
+DROP TABLE t;
